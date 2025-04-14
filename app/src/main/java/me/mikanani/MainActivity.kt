11a1d0ab5afb49faff8edc5afbc477826ed6944f
王丙瑞 <wangbr@meixing.com>
2025-04-14 17:26:38 +0800

    Init

---
+++ app/src/main/java/me/mikanani/MainActivity.kt
@@ -0,0 +1,55 @@
+package me.mikanani
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fleeksoft.ksoup.Ksoup
+import me.mikanani.ui.theme.MikananiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MikananiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+@Composable
+fun formatData(){
+    val doc: Document = Ksoup.parseGetRequestBlocking(url = "https://en.wikipedia.org/")
+
+    println("title: ${doc.title()}")
+    val headlines: Elements = doc.select("#mp-itn b a")
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MikananiTheme {
+        Greeting("Android")
+    }
+}
