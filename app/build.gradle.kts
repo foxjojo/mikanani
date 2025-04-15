3416dc059d195aaeb8334ce43513d2b15df37407
wangbr <wangbr@meixing.com>
2025-04-15 18:46:46 +0800

    换成正确的库

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,10 @@ android {
 }
 
 dependencies {
-    implementation(libs.kotlinx.coroutines.android)
-    implementation(libs.ksoup.korlibs)
-    implementation(libs.ksoup.network.korlibs)
+
+// Ksoup.parseFile, Ksoup.parseSource
+    implementation(libs.ksoup.kotlinx)
+    implementation(libs.ksoup.network)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
